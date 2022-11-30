b21136052c96576ad9f9db4ad5d4818a7383b12d
Alwin Vk <alwinvarghesek@gmail.com>
2022-11-30 11:19:58 +0000

    Completed Version

--- Furniture_app/ContentView.swift
+++ Furniture_app/ContentView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+   
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        
+        VStack {
+            HomeScreen()
+        }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+
+
+
+
+
